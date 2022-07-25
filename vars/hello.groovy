01d842ddc1abc8a2df4d29f52a1c8e852cecf223
haribabu0105 <haribabu0105@gmail.com>
2022-07-25 23:52:04 +0530

    kjbfytd

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,5 +1,5 @@
 def hello() {
-    echo ('hello world')
+    echo ('jb-hello-world-maven')
        git credentialsId: 'hari', url: 'git@github.com:haribabu0105/java-hello-world-with-maven.git'
     {
    return this
