6b7a0c724efe916b8a678592eedffb53d0f6e231
haribabu0105 <haribabu0105@gmail.com>
2022-07-28 14:51:25 +0530

    fghgfgg

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,5 +1,5 @@
 def checkout_git() {
-    echo" checkout code from git."
+    echo" take code from git."
       git credentialsId: 'hari', url: 'git@github.com:haribabu0105/hello-world-war.git'
 
 }
