232b14ab1b9cd17b778dfd303768357f78b424d0
haribabu0105 <haribabu0105@gmail.com>
2022-07-29 16:36:30 +0530

    gggg

--- vars/deploy_tomcat.groovy
+++ vars/deploy_tomcat.groovy
@@ -1,7 +1,9 @@
 def deploy_tomcat() {
     
     echo "deploying to tomcat server."
-   sh '''
+   sh """
       cd ${WORKSPACE}/shellscripts
       sh ./deploy_java.sh
-      '''    
+      """   
+}
+return this 
