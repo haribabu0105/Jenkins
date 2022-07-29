9bf5f1d41bee89505d4b9a6943fa4bc07e165c95
haribabu0105 <haribabu0105@gmail.com>
2022-07-29 16:59:26 +0530

    cc

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir /var/lib/jenkins/deploy
+ mkdir /var/lib/jenkins/deploy
 cd /var/lib/jenkins/deploy
 aws s3 cp s3://jenkinsbucket12345/java3/sparkjava-hello-world-1.0.war .
 scp sparkjava-hello-world-1.0.war ec2-user@20.0.0.9:/home/ec2-user/tomcat/apache-tomcat-9.0.65/webapps
