04e93fbf4e7e4bd043fb619d80bc3f40e260fb94
haribabu0105 <haribabu0105@gmail.com>
2022-08-26 17:56:17 +0530

    KGF

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -5,7 +5,7 @@
  IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
 mkdir /var/lib/jenkins/deploy
 cd /var/lib/jenkins/deploy
-aws s3 cp s3://jenkinsbucket1235/java3/$tag/sparkjava-hello-world-1.0.war .
+aws s3 cp s3://jenkins54321/java3/$tag/sparkjava-hello-world-1.0.war .
 scp sparkjava-hello-world-1.0.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.65/webapps
 ssh ec2-user@${IP} << HERE
     cd /home/ec2-user/tomcat/apache-tomcat-9.0.65/bin/
